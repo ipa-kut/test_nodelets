16527a2d67787cbfc0d1a709190c6484735473e1
kut <kut@ubuntu.p52.ipa>
2021-06-04 14:31:13 +0200

    add external and internal pub/sub example

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # test_nodelets
-Just playing yround with nodelets
+Just playing around with nodelets
+
+## test_nodelets/DiffTimeNodelet
+
+Startup: `roslaunch test_nodelets test_diff_time.launch`
+
+* Has multithreading example
+    * One thread saves current time into private member, another reads it out via mutexing
+* Has internal/external pub/sub example
+    * Creates two nodelets of same type
+    * User pubs int value to /in topic of one nodelet
+    * This value is then published internally via zero copy memory transfer to the other nodelet which then prints it out
+
+
+
+## Resources
+
+* [ROS Nodelet wiki](http://wiki.ros.org/nodelet) -> Just an overview
+* [ROS Nodelet tutorials](http://wiki.ros.org/nodelet/Tutorials/Porting%20nodes%20to%20nodelets) -> uselesse except for configs
+* [Packt hub](https://hub.packtpub.com/working-pluginlib-nodelets-and-gazebo-plugins/) -> Good overview and simple code example
+* [Kobuki nodelets](http://wiki.ros.org/kobuki/Tutorials/Write%20your%20own%20controller%20for%20Kobuki) -> Taken most parts from here
+* [Xlearpath robotics](https://www.clearpathrobotics.com/assets/guides/kinetic/ros/Nodelet%20Everything.html) -> Best example of multithreading
+
--- include/test_nodelets/calc_diff_time.h
+++ include/test_nodelets/calc_diff_time.h
@@ -2,7 +2,7 @@
 #define CALC_DIFF_TIME_H
 
 #include <ros/ros.h>
-#include <std_msgs/Bool.h>
+#include <std_msgs/Int32.h>
 #include <boost/thread.hpp>
 
 namespace calculators {
@@ -18,22 +18,36 @@ namespace calculators {
     void init()
     {
       ROS_INFO("Called init of class");
-      timer_ = nh_.createTimer(ros::Duration(1.0), boost::bind(& DiffTimeClass::timerCb, this, _1));
-      reader_ = nh_.createTimer(ros::Duration(1.0), boost::bind(& DiffTimeClass::readerCb, this, _1));
+      timer_ = nh_.createTimer(ros::Duration(5.0), boost::bind(& DiffTimeClass::timerCb, this, _1));
+      reader_ = nh_.createTimer(ros::Duration(5.0), boost::bind(& DiffTimeClass::readerCb, this, _1));
+      extern_sub_ = nh_.subscribe("extern_sub", 10, &DiffTimeClass::externSubCb, this);
+      internal_sub_ = nh_.subscribe("internal_sub", 10, &DiffTimeClass::internalSubCb, this);
+      internal_pub_ = nh_.advertise<std_msgs::Int32>("internal_pub", 10);
+      intern_msg_.reset(new std_msgs::Int32());
     }
 
     void timerCb(const ros::TimerEvent& event) {
       mutex_a.lock();
       time_ = ros::Time::now();
       mutex_a.unlock();
-      ROS_INFO_STREAM("Saved time at " << time_ );
+      ROS_INFO_STREAM(name_ << " Saved time at " << time_ );
     }
 
     void readerCb(const ros::TimerEvent& event) {
       mutex_a.lock();
       ros::Time local_time = time_;
       mutex_a.unlock();
-      ROS_INFO_STREAM("Read time as " << time_ );
+      ROS_INFO_STREAM(name_ << " Read time as " << time_ );
+    }
+
+    void externSubCb(const std_msgs::Int32::ConstPtr& msg) {
+      ROS_INFO_STREAM(name_ << " got external value " << msg->data );
+      intern_msg_->data = msg->data;
+      internal_pub_.publish(intern_msg_);
+    }
+
+    void internalSubCb(const std_msgs::Int32::ConstPtr& msg) {
+      ROS_INFO_STREAM(name_ << " got internal value " << msg->data );
     }
 
 
@@ -44,6 +58,10 @@ namespace calculators {
     ros::Timer timer_;
     ros::Timer reader_;
     ros::Time time_;
+    ros::Subscriber extern_sub_;
+    ros::Subscriber internal_sub_;
+    ros::Publisher internal_pub_;
+    boost::shared_ptr<std_msgs::Int32> intern_msg_;
 
   };
 
